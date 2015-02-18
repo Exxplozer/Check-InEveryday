032fe80e9cabc27f7c020a68ee0524df8e759799
Exxplozer <alias.co>
2015-02-18 12:28:46 +0200

    simple update

--- routes/checkin.js
+++ routes/checkin.js
@@ -1,27 +1,7 @@
 var config = require('../config');
 var checkin = require('../models/currentCheckin').Checkin;
-//var request = require('request');
 var mongoose = require('../lib/mongoose');
 var foursquare = require('node-foursquare')(config.get('foursquare'));
-
-/*
-exports.post = function(req,res,next) {
-    var uri = "https://api.foursquare.com/v2/checkins/add?clientId="+
-        config.get("foursquare:secrets:clientId")+
-        "&clientSecret="+
-        config.get("foursquare:secrets:clientSecret")+
-        "&v=20150216&venueId="+req.body.venueId+"&oauth_token="+req.body.oauth_token+"&ll="+req.body.ll;
-
-    console.log(uri);
-    request.post({
-        headers: {'content-type': 'application/json'},
-        url: uri,
-        json: null
-    }, function (error, response, body) {
-        if (error) return next(error);
-        console.log(body);
-    });
-};*/
 
 exports.get = function(req,res,next) {
     foursquare.Checkins.getRecentCheckins(null, req.params.token, function (error, data) {
