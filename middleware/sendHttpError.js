084a8455c6ea2825752794dbb9a868c8bd54a85d
Exxplozer <alias.co>
2015-02-18 14:02:47 +0200

    Commit description

--- middleware/sendHttpError.js
+++ middleware/sendHttpError.js
@@ -1,7 +1,6 @@
 module.exports = function(req, res, next) {
     res.sendHttpError = function(error) {
-        console.log(error.status);
-        console.log(error);
+        
         res.status(error.status);
         res.json(error);
     };
