6f1eb2398760fb551edfe53cc04c191adbf1a0df
o.paschenko <mailoleg4@gmail.com>
2015-04-09 19:25:06 +0300

    exception

--- routes/private/exception.js
+++ routes/private/exception.js
@@ -1,6 +1,7 @@
 var Exception = require('../../models/currentCheckin').Exception;
 
 exports.put = function (req, res, next) {
+    console.log(req.body.message + " " + req.body.targetSite);
       new Exception({
             message : req.body.message,
             Target : req.body.targetSite,
